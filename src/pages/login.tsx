c2e18f72fa2e3612cef63565b22160482d2c001c
ayush6999 <ayushjaiswal6999@gmail.com>
2024-05-06 15:48:01 +0530

    login/signup routes setup

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,47 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import { Label } from "./components/ui/label";
 import { Input } from "./components/ui/input";
 import { cn } from "@/utils/cn";
 
 function Login() {
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+
+  const [email , setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     console.log("Form submitted");
+
+    const res = await fetch('url', {
+      method : "POST",
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body :  JSON.stringify({email, password}),
+    })
+
   };
   return (
     <div className="bg-black flex justify-center items-center h-screen">
-      <div className="max-w-md w-full rounded-none md:rounded-2xl p-4 md:p-8 shadow-input bg-white dark:bg-black border border-white">
-        <h2 className="font-bold text-xl text-neutral-800 dark:text-neutral-200">
+      <div className="max-w-md w-full rounded-xl md:rounded-2xl p-4 md:p-8 shadow-input bg-black dark:bg-black border border-gray-800">
+        <h2 className="font-bold text-2xl text-white dark:text-neutral-200">
           Welcome to StreamYard
         </h2>
-        <p className="text-neutral-600 text-sm max-w-sm mt-2 dark:text-neutral-300">
+        <p className="text-white text-md max-w-sm mt-2 dark:text-neutral-300">
           Signup to Stream Yard if you don't have account
         </p>
 
-        <form className="my-8" onSubmit={handleSubmit}>
-          <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2 mb-4">
-            <LabelInputContainer>
-              <Label htmlFor="firstname">First name</Label>
-              <Input id="firstname" placeholder="Tyler" type="text" />
-            </LabelInputContainer>
-            <LabelInputContainer>
-              <Label htmlFor="lastname">Last name</Label>
-              <Input id="lastname" placeholder="Durden" type="text" />
-            </LabelInputContainer>
-          </div>
+        <form className="my-8">
           <LabelInputContainer className="mb-4">
-            <Label htmlFor="email">Email Address</Label>
-            <Input id="email" placeholder="projectmayhem@fc.com" type="email" />
+            <Label htmlFor="email" className="text-white">Email Address</Label>
+            <Input id="email" className="bg-black text-white" placeholder="projectmayhem@fc.com" type="email" onClick={(e) => {
+              setEmail(e.target.value);
+            }} />
           </LabelInputContainer>
           <LabelInputContainer className="mb-4">
-            <Label htmlFor="password">Password</Label>
-            <Input id="password" placeholder="••••••••" type="password" />
+            <Label htmlFor="password" className="text-white" >Password</Label>
+            <Input id="password" className="bg-black text-white" placeholder="••••••••" type="password" onClick={()=> {
+              setPassword(e.target.value);
+            }}/>
           </LabelInputContainer>
 
           <button
             className="mb-4 bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
             type="submit"
+            onClick={handleSubmit}
           >
+
             login &rarr;
             <BottomGradient />
           </button>
--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -1,46 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
 import { Label } from "./components/ui/label";
 import { Input } from "./components/ui/input";
 import { cn } from "@/utils/cn";
 
 function Signup() {
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    console.log("Form submitted");
+    console.log(fistName, lastName);
+
+    const res = await fetch('uro', {
+      method : "POST",
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body :  JSON.stringify({fistName, lastName, email, password}),
+    })
+
   };
+
+  const [fistName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
     <div className="bg-black flex justify-center items-center h-screen">
-      <div className="max-w-md w-full rounded-none md:rounded-2xl p-4 md:p-8 shadow-input bg-white dark:bg-black border border-white">
-        <h2 className="font-bold text-xl text-neutral-800 dark:text-neutral-200">
+      <div className="max-w-md w-full rounded-none md:rounded-2xl p-4 md:p-8 shadow-input bg-black dark:bg-black border border-white">
+      <h2 className="font-bold text-2xl text-white dark:text-neutral-200">
           Welcome to StreamYard
         </h2>
-        <p className="text-neutral-600 text-sm max-w-sm mt-2 dark:text-neutral-300">
-          Login to Stream Yard if you have already signup
+        <p className="text-white text-md max-w-sm mt-2 dark:text-neutral-300">
+          Signup to Stream Yard if you don't have account
         </p>
 
-        <form className="my-8" onSubmit={handleSubmit}>
+        <form className="my-8">
           <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2 mb-4">
             <LabelInputContainer>
-              <Label htmlFor="firstname">First name</Label>
-              <Input id="firstname" placeholder="Tyler" type="text" />
+              <Label htmlFor="firstname" className="text-white" >First name</Label>
+              <Input id="firstname" className="bg-black text-white" placeholder="Tyler" type="text" onChange={(e)=> {
+                setFirstName(e.target.value);
+              }} />
             </LabelInputContainer>
             <LabelInputContainer>
-              <Label htmlFor="lastname">Last name</Label>
-              <Input id="lastname" placeholder="Durden" type="text" />
+              <Label htmlFor="lastname" className="text-white">Last name</Label>
+              <Input id="lastname" className="bg-black text-white"  placeholder="Durden" type="text" onChange={(e) => {
+                setLastName(e.target.value);
+              }}/>
             </LabelInputContainer>
           </div>
           <LabelInputContainer className="mb-4">
-            <Label htmlFor="email">Email Address</Label>
-            <Input id="email" placeholder="projectmayhem@fc.com" type="email" />
+            <Label htmlFor="email" className="text-white">Email Address</Label>
+            <Input id="email" className="bg-black text-white" placeholder="projectmayhem@fc.com" type="email" onChange={(e)=>{
+              setEmail(e.target.Value);
+            }}/>
           </LabelInputContainer>
           <LabelInputContainer className="mb-4">
-            <Label htmlFor="password">Password</Label>
-            <Input id="password" placeholder="••••••••" type="password" />
+            <Label htmlFor="password" className="text-white" >Password</Label>
+            <Input id="password" className="bg-black text-white" placeholder="••••••••" type="password" onChange={(e)=> {
+              setPassword(e.target.value);
+            }}/>
           </LabelInputContainer>
 
           <button
             className="mb-4 bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
             type="submit"
+            onClick={handleSubmit}
           >
             Sign up &rarr;
             <BottomGradient />
